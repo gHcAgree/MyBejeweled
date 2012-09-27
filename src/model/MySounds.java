70cef85115d60d16f66076027e79252b81559796
gHcAgree <guiggle90@gmail.com>
2012-09-27 18:24:43 +0800

    init

---
+++ src/model/MySounds.java
@@ -0,0 +1,25 @@
+package model;
+
+public class MySounds {
+	public static final String DIR           = "sounds/";
+	public static final String AWESOME       = "awesome.wav";
+	public static final String BEGIN         = "begin.wav";
+	public static final String BGM1          = "BGM1.mid";
+	public static final String BGM2          = "BGM2.mid";
+	public static final String BOMB          = "bomb.wav";
+	public static final String CLEAR         = "clear.wav";
+	public static final String CLICK         = "click.wav";
+	public static final String CLICKINBUTTON = "clickinbutton.wav";
+	public static final String DELETE        = "delete.wav";
+	public static final String GAMEOVER      = "gameover.wav";
+	public static final String GOOD          = "good.wav";
+	public static final String HINT          = "hint.wav";
+	public static final String LEVEL         = "level.wav";
+	public static final String NEWHIGHSCORE  = "newhighscore.wav";
+	public static final String RING          = "ring.wav";
+	public static final String SWAP          = "swap.wav";
+	public static final String TIMEOUT       = "timeout.wav";
+	public static final String UNBELIEVABLE  = "unbelievable.wav";
+	public static final String WARNING       = "warning.wav";
+	public static final String WIN           = "win.wav";
+}
